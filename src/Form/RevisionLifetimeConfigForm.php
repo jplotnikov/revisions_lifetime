446e9ea4c83e4832cfea73a8e59dbf8348beb056
Evgeniy Plotnikov <evgeniy.plotnikov@propeople.com.ua>
2017-07-07 14:55:57 +0300

    Add revision lifetime module to repository

---
+++ src/Form/RevisionLifetimeConfigForm.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Drupal\pfqaplatform_revisions_lifetime\Form;
+
+/**
+ * @file
+ * Contains Drupal\pfqaplatform_revisions_lifetime\Form\RevisionLifetimeConfigForm.
+ */
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class SettingsForm.
+ *
+ * @package Drupal\pfqaplatform_revisions_lifetime\Form
+ */
+class RevisionLifetimeConfigForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'pfqaplatform_revisions_lifetime.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = \Drupal::config('pfqaplatform_revisions_lifetime.settings');
+
+    // Get all Content Types.
+    $content_types = \Drupal::entityTypeManager()
+      ->getStorage('node_type')
+      ->loadMultiple();
+
+    foreach ($content_types as $key => $ct) {
+      $content_types_items[$key] = $ct->label();
+    }
+
+    // Build form.
+    $form['content_types'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Content types:'),
+      '#description' => $this->t('Content types than will be removed. If nothing is selected, then the revisions of all content types will be deleted'),
+    ];
+    $form['content_types']['content_types_items'] = array(
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Select Content types:'),
+      '#options' => $content_types_items,
+      '#default_value' => $config->get('content_types_items'),
+    );
+
+    $form['revisions_age'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Time period:'),
+      '#description' => $this->t('Revisions older than the selected age, will be removed'),
+    ];
+    $time_period = [
+      '3600' => $this->t('Hours'),
+      '86400' => $this->t('Days'),
+      '604800' => $this->t('Weeks'),
+      '2592000' => $this->t('Month'),
+    ];
+    $form['revisions_age']['period'] = array(
+      '#type' => 'select',
+      '#title' => $this->t('Items:'),
+      '#options' => $time_period,
+      '#default_value' => $config->get('period'),
+    );
+    $form['revisions_age']['quantity_items'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Quantity items:'),
+      '#default_value' => $config->get('quantity_items'),
+      '#min' => 1,
+    ];
+
+    // Delete all old revision for checked content types.
+    $form['delete_old_revision'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Delete all revisions'),
+      '#description' => $this->t('Delete all old revision for checked content types'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    // Delete all old revision.
+    if ($form_state->getValue('delete_old_revision')) {
+      $delete = \Drupal::service('pfqaplatform_revisions_lifetime.revision_cleanup');
+      $delete->deleteOldRevision();
+    }
+
+    // Save settings for content types and lifetime.
+    $config = \Drupal::configFactory()->getEditable('pfqaplatform_revisions_lifetime.settings')
+      ->set('period', $form_state->getValue('period'))
+      ->set('quantity_items', $form_state->getValue('quantity_items'))
+      ->set('content_types_items', $form_state->getValue('content_types_items'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
---
+++ src/RevisionLifetimeCleanup.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\pfqaplatform_revisions_lifetime;
+
+/**
+ * @file
+ * Contains Drupal\pfqaplatform_revisions_lifetime\PfqaplatformRevisionsLifetime.
+ */
+
+use Drupal\Core\Database\Database;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+
+/**
+ * Class RevisionLifetimeCleanup.
+ */
+class RevisionLifetimeCleanup {
+
+  /**
+   * An instance of the "entity_type.manager" service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The config object.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * Constructs this PfqaplatformRevisionsLifetime object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Instance of the "entity_type.manager" service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config
+   *   The configuration factory object to use.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ConfigFactoryInterface $config) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->config = $config;
+  }
+
+  /**
+   * Delete old revisions.
+   */
+  public function deleteOldRevision() {
+    // Get period, quantity items from config and calculate delete age.
+    $period = $this->config->get('pfqaplatform_revisions_lifetime.settings')->get('period');
+    $quantity_items = $this->config->get('pfqaplatform_revisions_lifetime.settings')->get('quantity_items');
+    $deleted_age = time() - $period * $quantity_items;
+
+    // Get delete content types from config.
+    $content_types = $this->config->get('pfqaplatform_revisions_lifetime.settings')->get('content_types_items');
+
+    // Selection of revisions suitable for the condition.
+    $revisions = Database::getConnection()->select('node_revision', 'r')
+      ->fields('r', array('nid', 'vid'));
+    $revisions->addJoin('left','node','n','r.nid=n.nid');
+    $revisions->fields('n', array('vid'));
+    $revisions->condition('n.type', $content_types, 'IN');
+    $revisions->condition('r.revision_timestamp', $deleted_age, '<');
+    $data = $revisions->execute();
+    $result = $data->fetchAll(\PDO::FETCH_OBJ);
+
+    if (!empty($result)) {
+      foreach ($result as $value) {
+        // Delete revisions if it is not the last.
+        if ($value->vid != $value->n_vid) {
+          $this->entityTypeManager->getStorage('node')->deleteRevision($value->vid);
+        }
+      }
+    }
+  }
+
+}
